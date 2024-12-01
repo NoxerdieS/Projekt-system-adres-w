9b14f3986f287b3581414e19eeaf231840c6f082
Mateusz <mbonat42@gmail.com>
2024-12-01 21:05:09 +0100

    add database schema

--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+struct Interest {
+    int id;
+
+    string name;
+};
+
+struct Person {
+    int id;
+
+    string name;
+    string surname;
+    string address;
+    string email;
+};
+
+struct PersonHasInterests {
+    int personId;
+    int interestId;
+};
+
+struct PrimaryKeyMetadata {
+    int lastPersonId;
+    int lastInterestId;
+};
+
+struct Database {
+    PrimaryKeyMetadata metadata;
+
+    Interest[] interests;
+    Person[] people;
+    PersonHasInterests[] personHasInterests;
+};
+
+Database* LoadDatabase() {
+    // TODO
+}
+
+void SaveDatabase(Database database) {
+    // TODO
+}
 
 int main() {
     system("chcp 65001>>null");
