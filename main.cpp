699e27b1614d9c4ce4c74945283b9c178b71df7c
Mateusz Bonat <mbonat42@gmail.com>
2024-12-12 19:22:57 +0100

    Update

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 
 using namespace std;
+
+int peopleCount = 0;
 
 struct Person {
     int id;
@@ -19,13 +22,88 @@ struct Database {
     Person* people = new Person[100];
 };
 
-void LoadDatabase(Database* database) {
+
+void AddPerson(Database* database, Person person, int peopleCount) {
+    database->people[database->peopleCount] = person;
+    database->peopleCount++;
+}
+
+string[] SplitStringByDelimiter(string payload, char delimiter) {
+    string[] splitted = new string[2];
+
+    for (int i = 0; i < payload.length(); i++) {
+        if (payload[i] == delimiter) {
+            splitted[0] = payload.substr(0, i);
+            payload = payload.substr(i + 1, payload.length() - i - 1);
+            i = 0;
+        }
+    }
+
+    return splitted;
+}
+
+void LoadDatabase(Database* database, int peopleCount) {
+    ifstream file("database.csv");
+
+    if (!file) {
+        cerr << "Wystąpił błąd podczas odczytywania bazy danych." << endl;
+        exit(1);
+    }
+
     database->nextPersonId = 0;
+    database->peopleCount = 0;
     database->people = new Person[2];
+
+    string line;
+    while (getline(file, line)) {
+        string[] splittedLine = SplitStringByDelimiter(line, ';');
+
+        if (splittedLine.length() != 6) {
+            cerr << "Niepoprawny format wiersza w bazie danych: \"" << line << "\"" << endl;
+            exit(1);
+        }
+
+        loadedPerson = new Person();
+        loadedPerson->id = stoi(splittedLine[0]); // wywali błąd
+        loadedPerson->name = splittedLine[1];
+        loadedPerson->surname = splittedLine[2];
+        loadedPerson->address = splittedLine[3];
+        loadedPerson->hobbies = SplitStringByDelimiter(splittedLine[4], ',');
+
+        AddPerson(database, loadedPerson);
+    }
+
+    // sprawdź czy ID nie są zdublowane
+    for (int i = 0; i < sizeof(database->people); i++) {
+        for (int j = 0; j < sizeof(database->people); j++) {
+            if (j != 0) { // żeby nie sprawdzać tego samego ID
+                if (database->people[i]->id == database->people[j]->id) {
+                    cerr << "Zduplikowane ID w bazie danych: " << database->people[i]->id << endl;
+                    exit(1);
+                }
+            }
+        }
+    }
 }
 
 void SaveDatabase(Database* database) {
-    // TODO
+    ofstream file("database.csv");
+
+    if (!file) {
+        cerr << "Wystąpił błąd podczas zapisywania bazy danych." << endl;
+        exit(1);
+    }
+
+    for (int i = 0; i < sizeof(database->people); i++) {
+        file << database->people[i]->id << ";";
+        file << database->people[i]->name << ";";
+        file << database->people[i]->surname << ";";
+        file << database->people[i]->address << ";";
+        for (int j = 0; j < sizeof(database->people[i]->hobbies); j++) {
+            file << database->people[i]->hobbies[j] << ",";
+        }
+        file << endl;
+    }
 }
 
 int MenuDisplay() {
@@ -58,10 +136,8 @@ int MenuDisplay() {
 }
 
 int main() {
-    Database* database = new Database();
-    LoadDatabase(database);
-
     system("chcp 65001>>null");
-    cout << "buh" << endl;
+    Database* database = new Database();
+    LoadDatabase(database, peopleCount);
     return 0;
 }
